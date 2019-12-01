fae13c571666a04973664d6e4338ff75c42c46da
Christian <christianmcfa@gmail.com>
2019-12-01 16:27:58 -0500

    Adding material-ui text box to contact page and changed layout of homepage

--- WebApplication1/ClientApp/src/components/Contact.js
+++ WebApplication1/ClientApp/src/components/Contact.js
@@ -1,4 +1,7 @@
 ﻿import React, { Component } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 export class Contact extends Component {
     static displayName = Contact.name;
@@ -9,11 +12,28 @@ export class Contact extends Component {
                 <h1>Contact Me</h1>
                 <p>Feel free to check out any of following:</p>
                 <ul>
-                    <li><a href='https://github.com/christianmcfar' target="_blank"
-                        rel="noopener noreferrer">GitHub</a></li>
-                    <li><a href='https://www.linkedin.com/in/christian-mcfarland/' target="_blank"
-                        rel="noopener noreferrer">LinkedIn</a></li>
+                    <li>
+                        <a href='https://github.com/christianmcfar' target="_blank" rel="noopener noreferrer">GitHub</a>
+                    </li>
+                    <li>
+                        <a href='https://www.linkedin.com/in/christian-mcfarland/' target="_blank" rel="noopener noreferrer">LinkedIn</a>
+                    </li>
                 </ul>
+                <p>Or send me an email here!</p>
+                <TextField
+                    id="outlined-multiline-static"
+                    label="Email"
+                    multiline
+                    rows="4"
+                    margin="normal"
+                    variant="outlined"
+                    fullWidth
+                />
+                <Grid container justify="center">
+                    <Button variant="contained" color="primary">
+                        Send
+                    </Button>
+                </Grid>
             </div>
         );
     }
--- WebApplication1/ClientApp/src/components/Home.js
+++ WebApplication1/ClientApp/src/components/Home.js
@@ -6,64 +6,63 @@ export class Home extends Component {
     render () {
         return (
         <div>
-            <div className="homepageimage" style={{ width: '100%', height: '100%' }}>
-                <div className="whitetext container center-ver">
-                    <h1>Hello, world!</h1>
-                    <p>Welcome to your new single-page application, built with:</p>
-                    <ul>
-                      <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-                      <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-                      <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-                    </ul>
-                    <p>To help you get started, we have also set up:</p>
-                    <ul>
-                      <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-                      <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-                      <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-                    </ul>
-                    <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+            <div>
+                <div className="homepageimage">
+                    <div className="whitetext container center">
+                        <h1>Christian McFarland</h1>
+                        <h5>
+                            Welcome to my web app! This application was built on my quest of continuously learning. 
+                            I'm a recent graduate of Wilfrid Laurier University and when I'm not trekking the outdoors 
+                            I'm looking to hone my skills, with a current focus on gaining a better understanding of web development.
+                        </h5>
+                    </div>
                 </div>
             </div>
             <div className="black">
                 <div className="whitetext container center-ver">
-                    <h1>Objective/About Me</h1>
-                    <p>
-                        Welcome to your new single-page application, built with: 
-                        <a href='https://get.asp.net/'> ASP.NET Core</a> and 
-                        <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'> C#</a> for cross-platform server-side code 
-                        <a href='https://facebook.github.io/react/'> React</a> for client-side code 
-                        <a href='http://getbootstrap.com/'> Bootstrap</a> for layout and styling 
-                        To help you get started, we have also set up: 
-                        <strong> Client-side navigation</strong>. For example, click <em> Counter</em> then <em> Back</em> to return here. 
-                        <strong> Development server integration</strong>. In development mode, the development server from 
-                        <code> create-react-app</code> runs in the background automatically, so your client-side resources are dynamically 
-                        built on demand and the page refreshes when you modify any file. 
-                        <strong> Efficient production builds</strong>. In production mode, development-time features are disabled, and your 
-                        <code> dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files. 
-                        The <code> ClientApp</code> subdirectory is a standard React application based on the <code> create-react-app </code> 
-                        template. If you open a command prompt in that directory, you can run <code> npm</code> commands such as 
-                        <code> npm test</code> or <code> npm install</code>.
-                    </p>
+                    <h1>Projects</h1>
+                    <ul>
+                        <li>
+                            <strong>Personal Web App</strong>
+                            <p>
+                                My first dive into the world of web developement. 
+                                This application was built with:
+                                <a href='https://get.asp.net/' target="_blank" rel="noopener noreferrer"> ASP.NET Core</a> and
+                                <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx' target="_blank" rel="noopener noreferrer"> C#</a> for cross-platform server-side code,
+                                <a href='https://facebook.github.io/react/' target="_blank" rel="noopener noreferrer"> React</a> for client-side code, and
+                                <a href='http://getbootstrap.com/' target="_blank" rel="noopener noreferrer"> Bootstrap</a> and
+                                <a href='https://material-ui.com/' target="_blank" rel="noopener noreferrer"> Material-UI</a> for layout and styling.
+                                Currently working on adding email feautres.
+                            </p>
+                        </li>
+                        <li>
+                            <strong>N-Queens Solution</strong>
+                            <p>
+                                Solution to 
+                                <a href='https://en.wikipedia.org/wiki/Eight_queens_puzzle' target="_blank" rel="noopener noreferrer"> N-Queens </a> 
+                                problem for chess board of up to 1 million queens using a 
+                                <a href='https://en.wikipedia.org/wiki/Min-conflicts_algorithm' target="_blank" rel="noopener noreferrer"> Min-Conflicts </a> 
+                                algorithm in C++, found on
+                                <a href='https://github.com/christianmcfar/nqueens' target="_blank" rel="noopener noreferrer"> GitHub</a>. 
+                                Currently working on visualizer to display solving in real time.
+                            </p>
+                        </li>
+                        <li>
+                            <strong>3D Rush Hour Game</strong>
+                            <p>
+                                Implemented a 3D Rush Hour game in C++ using
+                                <a href='https://www.opengl.org/' target="_blank" rel="noopener noreferrer"> OpenGL </a> 
+                                to render graphics and create lighting.
+                            </p>
+                        </li>
+                    </ul>
                 </div>        
             </div>
-            <div>
+            <div className="white">
                 <div className="container center-ver">
-                    <h1>If only I could be so grossly incandescent!</h1>
+                    <h1>Coming soon!</h1>
                     <p>
-                        Now that I am Undead, I have come to this great land, the birthplace of Lord Gwyn, to seek my very own sun! 
-                        The way I see it, our fates appear to be intertwined.
-                        In a land brimming with Hollows, could that really be mere chance?
-                        So, what do you say? Why not help one another on this lonely journey?
-                        We are amidst strange beings, in a strange land.
-                        The flow of time itself is convoluted; with heroes centuries old phasing in and out.
-                        The very fabric wavers, and relations shift and obscure.
-                        There's no telling how much longer your world and mine will remain in contact.
-                        But, use this, to summon one another as spirits, cross the gaps between the worlds, and engage in jolly co-operation!
-                        Of course, we are not the only ones engaged in this.
-                        But I am a warrior of the sun! Spot my summon signature easily by its brilliant aura.
-                        If you miss it, you must be blind!
-                        Oh, hello there. I will stay behind, to gaze at the sun.
-                        The sun is a wondrous body. Like a magnificent father!
+                        Still working on this part. Check back later for more to come!  
                     </p>
                 </div>  
             </div>
